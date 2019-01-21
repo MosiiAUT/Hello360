245421061450a7f229a561322003262391ad7e12
MosiiAUT <a.moser95@hotmail.com>
2019-01-21 14:24:05 +0100

    Super aussehen, super Funktionen
    alles palleti

--- DescriptionUI.js
+++ DescriptionUI.js
@@ -172,7 +172,7 @@ const styles = StyleSheet.create({
         // Fill the entire surface
         width: 900,
         height: 700,
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
+        backgroundColor: 'rgba(255, 255, 255, 0.6)',
         //justifyContent: 'center',
         alignItems: 'stretch',
     },
